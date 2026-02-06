593f6992bc8356ad6a66d920ea834c5466f7fd46
ShresthSamyak <shresthsamyak@example.com>
2026-02-07 01:05:24 +0530

    Install deps at startup - ZIP Deploy workaround

--- startup.sh
+++ startup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd backend
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8000
+
+# Install dependencies if not already installed
+pip install -r ../requirements.txt --user --quiet --no-warn-script-location
+
+# Start the app
+python -m gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8000
